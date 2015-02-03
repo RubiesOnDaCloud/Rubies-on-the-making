b6b60eb4337da65e189f4b71db788bea2b886a11
imeldahalim <imelda.halim@gmail.com>
2015-02-03 20:56:40 +0100

    Create 04_fullname.rb

---
+++ 04_fullname.rb
@@ -0,0 +1,8 @@
+puts 'Hello there, and what\'s your first name?'
+first_name = gets.chomp
+puts 'What\'s your middle name?'
+middle_name = gets.chomp
+puts 'What\'s your last name?'
+last_name = gets.chomp
+puts 'Your name is '+ first_name +' '+ middle_name +' '+ last_name +''  '? What a lovely name!'
+puts 'Pleased to meet you, '+ first_name +' '+ middle_name +' '+ last_name + '. :)'
